ba7ce7d04656924f4a80e9d27a88385ccddef2e7
Sylvain Laurent <s@6120.eu>
2023-04-12 04:00:32 +0200

    Add does-not-seem-to-be-working-buffering-to-avoid-flickering

--- script.sh
+++ script.sh
@@ -4,8 +4,9 @@
 
 # Constants
 API_URL="https://polygon-mainnet.infura.io/v3/${API_KEY}"
-TMP_IMG_FILE="./img.svg"
-IMG_FILE_FORMAT="./img-%s.svg"
+TMP_IMG_FILE1="./img1.svg"
+TMP_IMG_FILE2="./img2.svg"
+IMG_FILE_FORMAT="/tmp/img-%s.svg"
 INTERVAL=4
 
 # Request payload
@@ -15,7 +16,7 @@ REQUEST_DATA='{
   "params": [
     {
       "from": "0x0000000000000000000000000000000000000000",
-      "to":   "0xccdaf00c1782f3af7f841990941366274570c354",
+      "to": "0xccdaf00c1782f3af7f841990941366274570c354",
       "data": "0xc87b56dd0000000000000000000000000000000000000000000000000000000000000005"
     },
     "latest"
@@ -23,9 +24,10 @@ REQUEST_DATA='{
   "id": 1
 }'
 
+toggle=1
 while true; do
-  # Move the image and timestamp it
-  # mv "${TMP_IMG_FILE}" "$(printf "${IMG_FILE_FORMAT}" "$(date +%s)")"
+  # Choose the image file to update
+  TMP_IMG_FILE=$([ "$toggle" -eq "1" ] && echo "$TMP_IMG_FILE1" || echo "$TMP_IMG_FILE2")
 
   # Fetch image data and update the image file
   curl -s "${API_URL}" \
@@ -41,11 +43,9 @@ while true; do
   # Move the temporary file
   mv "${TMP_IMG_FILE}.tmp" "${TMP_IMG_FILE}"
 
+  # Toggle between image file names
+  toggle=$((1-toggle))
+
   # Sleep for the specified interval
   sleep "${INTERVAL}"
-done
- while true; do mv /tmp/img.svg "/tmp/img-`date +%s`.svg"; curl -s https://polygon-mainnet.infura.io/v3/"${YOUR_API_KEY}" \
-    -X POST \
-    -H "Content-Type: application/json" \
-    -d '{"jsonrpc":"2.0","method":"eth_call","params": [{"from": "0x0000000000000000000000000000000000000000","to": "0xccdaf00c1782f3af7f841990941366274570c354","data": "0xc87b56dd0000000000000000000000000000000000000000000000000000000000000005"}, "latest"],"id":1}' | jq  .result | xxd -r -p | cut -d, -f2 | base64 -d -i | jq -r .image | tee /tmp/img.svg; sleep 4;
 done
