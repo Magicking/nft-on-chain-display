34f77f33401c23f83de71b33ce250984c09d8322
Sylvain Laurent <s@6120.eu>
2023-04-14 19:00:10 +0200

    Update display

--- script.sh
+++ script.sh
@@ -8,38 +8,25 @@ TMP_IMG_FILE1="./img1.svg"
 TMP_IMG_FILE2="./img2.svg"
 IMG_FILE_FORMAT="/tmp/img-%s.svg"
 INTERVAL=4
-
-# Request payload
-REQUEST_DATA='{
-  "jsonrpc": "2.0",
-  "method": "eth_call",
-  "params": [
-    {
-      "from": "0x0000000000000000000000000000000000000000",
-      "to": "0xccdaf00c1782f3af7f841990941366274570c354",
-      "data": "0xc87b56dd0000000000000000000000000000000000000000000000000000000000000005"
-    },
-    "latest"
-  ],
-  "id": 1
-}'
+TOKEN_ADDR="0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03"
+TOKEN_ID=120
 
 toggle=1
 while true; do
   # Choose the image file to update
   TMP_IMG_FILE=$([ "$toggle" -eq "1" ] && echo "$TMP_IMG_FILE1" || echo "$TMP_IMG_FILE2")
 
-  # Fetch image data and update the image file
-  curl -s "${API_URL}" \
-    -X POST \
-    -H "Content-Type: application/json" \
-    -d "${REQUEST_DATA}" | \
-  jq .result | \
-  xxd -r -p | \
+  cd sl-nft-pixel-art-proto-2
+  # Write image.b64uri file
+  forge script --fork-url "${ALCHEMY_API_URL}" scripts/generateSVG.sol && \
+  cat image.b64uri | \
   cut -d, -f2 | \
   base64 -d -i | \
-  jq -r .image > "${TMP_IMG_FILE}.tmp"
+  jq -r .image | \
+  cut -d, -f2 | \
+  base64 -d -i > "../${TMP_IMG_FILE}.tmp"
 
+  cd ..
   # Move the temporary file
   mv "${TMP_IMG_FILE}.tmp" "${TMP_IMG_FILE}"
 
